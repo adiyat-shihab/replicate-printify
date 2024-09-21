9d69234fb02008aecaf056456193965bc447e5da
adiyat-shihab <mohammadshihab104@gmail.com>
2024-09-22 03:47:25 +0600

    project initilize

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import clothAnimation from "/src/assets/cloth.json";
+import Lottie from "lottie-react";
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      "text-5xl": "3.45rem",
+      "text-xl": "1.11875rem",
+      "text-2xl": "1.4875rem",
+      "text-3xl": "1.9625rem",
+      "text-4xl": "2.4125rem",
+    },
+  },
+  plugins: [],
+};
